8ad62df9aadbb8aca9b165a2e6a5dd2717092dce
Fidgetboyrolly <fidgetboyrolly@icloud.com>
2026-06-15 15:18:37 -0700

    Update Blockbench pmat plugin.js

--- Blockbench pmat plugin.js
+++ Blockbench pmat plugin.js
@@ -1,74 +1,69 @@
-(function() {
+Plugin.register("pmat_exporter", {
+    title: "Polymat Exporter",
+    icon: "icon-export",
+    author: "Rowen",
+    description: "Exports models to .pmat triangle format",
+    version: "1.0.0",
+    min_version: "4.0.0",
 
-    Plugin.register("rowen_pmat_exporter", {
-        title: "Polymat (.pmat) Exporter",
-        icon: "icon-export",
-        author: "Copilot",
-        description: "Exports models into Rowen's Polymat (.pmat) triangle format",
-        version: "1.0.0",
-        min_version: "4.0.0",
+    onload() {
 
-        onload() {
+        new Action("export_pmat", {
+            name: "Export as .pmat",
+            icon: "icon-export",
+            category: "file",
+            click() {
 
-            new Action("export_pmat", {
-                name: "Export as .pmat (Polymat)",
-                icon: "icon-export",
-                category: "file",
-                click() {
+                let triangles = [];
 
-                    let triangles = [];
+                Project.meshes.forEach(mesh => {
+                    mesh.faces.forEach(face => {
 
-                    Project.meshes.forEach(mesh => {
-                        mesh.faces.forEach(face => {
+                        let verts = face.getSortedVertices();
+                        let triList = [];
 
-                            let verts = face.getSortedVertices();
-                            let triList = [];
+                        if (verts.length === 3) {
+                            triList.push(verts);
+                        } else if (verts.length === 4) {
+                            triList.push([verts[0], verts[1], verts[2]]);
+                            triList.push([verts[0], verts[2], verts[3]]);
+                        }
 
-                            if (verts.length === 3) {
-                                triList.push(verts);
-                            } else if (verts.length === 4) {
-                                triList.push([verts[0], verts[1], verts[2]]);
-                                triList.push([verts[0], verts[2], verts[3]]);
+                        triList.forEach(tri => {
+
+                            let triData = tri.map(v => [v.x, v.y, v.z]);
+
+                            let colorInfo;
+
+                            if (face.texture) {
+                                colorInfo = "t:" + face.texture.name;
+                            } else if (face.color) {
+                                colorInfo = [face.color.h, face.color.s, face.color.v];
+                            } else {
+                                colorInfo = [0, 0, 1];
                             }
 
-                            triList.forEach(tri => {
-
-                                let triData = tri.map(v => [v.x, v.y, v.z]);
-
-                                let colorInfo;
-
-                                if (face.texture) {
-                                    colorInfo = "t:" + face.texture.name;
-                                } else if (face.color) {
-                                    colorInfo = [face.color.h, face.color.s, face.color.v];
-                                } else {
-                                    colorInfo = [0, 0, 1];
-                                }
-
-                                triData.push(colorInfo);
-                                triangles.push(triData);
-                            });
-
+                            triData.push(colorInfo);
+                            triangles.push(triData);
                         });
+
                     });
+                });
 
-                    Blockbench.export({
-                        type: "PMAT",
-                        extensions: ["pmat"],
-                        name: Project.name + ".pmat",
-                        content: JSON.stringify(triangles)
-                    });
+                Blockbench.export({
+                    type: "PMAT",
+                    extensions: ["pmat"],
+                    name: Project.name + ".pmat",
+                    content: JSON.stringify(triangles)
+                });
 
-                }
-            });
+            }
+        });
 
-            MenuBar.addAction("export_pmat", "file.export");
-        },
+        MenuBar.addAction("export_pmat", "file.export");
+    },
 
-        onunload() {
-            MenuBar.removeAction("export_pmat");
-        }
-
-    });
-
-})();
+    onunload() {
+        MenuBar.removeAction("export_pmat");
+    }
+});
